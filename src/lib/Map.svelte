dcfd7600ae0ee47f11b9368f51553060f3240704
emily-sakaguchi <122579546+emily-sakaguchi@users.noreply.github.com>
2023-11-08 12:19:55 -0500

    add_CMAs

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -2,6 +2,7 @@
 
 	import { onMount } from 'svelte';
 	import maplibregl from 'maplibre-gl';
+	import cma2021dbf from '../data/cma-2021-dbf.geo.json';
 
 	let pageHeight;
 	let pageWidth;
@@ -51,13 +52,54 @@
 				'raster-saturation': -1,
 				'raster-opacity': 0.42
 			}
-		})	
+		});	
 		
+		map.addSource('cma2021dbf', {
+			'type': 'geojson',
+			'data': cma2021dbf
+		});
+		map.addLayer({
+			'id': 'cma2021dbf',
+			'type': 'line',
+			'source': 'cma2021dbf',
+			'paint': {
+				'line-color': '#1E3765',
+				'line-width' : 3,
+				'line-opacity': 1
+		}
 	});
 
+	// map.addSource('torontoPM2021', {
+	// 	'type': 'tiff',
+	// 	'data': torontoPM2021
+	// });
+	// map.addLayer({
+	// 	'id': 'torontoPM2021',
+	// 	'type': 'raster',
+	// 	'source': 'torontoPM2021',
+	// });
+	
+map.on("load", function () {
+  map.loadImage("2021_Toronto_PM25.tif", function (error, image) {
+    if (error) throw error;
+
+    map.addImage("2021_Toronto_PM25.tif", image);
+
+    map.addLayer({
+      id: "TorontoPM25",
+      type: "raster",
+      source: "2021_Toronto_PM25.tif",
+      paint: {
+        "raster-opacity": 0.8, // Adjust opacity
+      },
+    });
+  });
+});
+
+});
+
+
 </script>
-
-
 
 
 <svelte:window bind:innerHeight={pageHeight} bind:innerWidth={pageWidth}/>
@@ -65,7 +107,6 @@
 <div id="map" style="height: {mapHeight}px"></div>
 
 <p>Data Sources: OpenStreetMap</p>
-
 
 
 
