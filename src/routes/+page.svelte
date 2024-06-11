ca1220d214015a6730cf27a7b5237855a2b22f29
Jeff Allen <jeffallen89@gmail.com>
2024-06-11 14:10:54 -0400

    updating text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,60 +35,93 @@
 		<div class="title">
 			<h1>Air Pollution and Premature Mortality </h1>
 			<p><b><u>Research</u></b>: Sabrina Chiodo; Lief Pagalan; Mackenzie Hurst; Meghan O’Neill; Helen Stylianou; Lori M. Diemert; Hong Chen; Jeffrey R. Brook; Andy Hong; Laura C. Rosella<br>
-			<b><u>Writing</u></b>: Konstantine Diochnos
+			<b><u>Writing</u></b>: Kosta Diochnos
 			<br>
 			<b><u>Maps & Graphics</u></b>: Jeff Allen, Emily Sakaguchi</p>
 			<p>Funded by a <a href="https://schoolofcities.utoronto.ca/programs-opportunities/urban-challenge-grant/" target = 'blank'>School of Cities Urban Challenge Grant</a>. Read the full paper here.</p>
 		</div>
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Climate change has been identified as the largest global health threat of the 21st century, and one of the most important measures that can be taken to lessen its toll on human health is to reduce air pollution. Cities are home to most of the world’s population and account for <a href="https://www.nature.com/articles/525179a">more than 70%</a> of global greenhouse gas (GHG) emissions. 
 		</p>
 		<p>
-			Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+			With increasing urbanization, traffic, economic activity, and extreme weather effects like smoke from wildfires, cities become areas of elevated air pollution. In Canada, <a href="https://canue.ca/pm-2-5-what-it-is-and-why-it-matters-february-24th-2022/" target="_blank">86% of the population</a> lives in areas where outdoor air pollutants exceed standard limits set by the World Health Organization. Health Canada has estimated that 15,300 premature deaths (i.e. deaths that occur before age 75) occur annually as a direct result of air pollution from human activity (Health Canada, 2022). 
+		</p>
+		<p>
+			To understand the impact of reducing air pollution on premature deaths, we used a tool called the <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7640636/" target="_blank">Mortality Population Risk Tool (PreMPoRT)</a> to simulate how reductions in nitrogen dioxide (NO2) and fine particulate matter less than 2.5 μm in diameter (PM2.5) would impact premature mortality in 31 Canadian Census Metropolitan Areas (CMAs). PM2.5 is a main component of harmful air pollution, while NO2 is another pollutant that can exacerbate the effects of air pollution on human health.
+		</p>
+		<p>
+			Our results show that interventions aimed at reducing PM2.5 and NO2 could decrease the number of premature deaths for Canadian cities. The greater the air pollution intervention, the greater the reduction in predicted premature deaths.
 		</p>
 
-		<h3>Total Lives Saved By Reducing Air Pollution</h3>
+		<h3>Total Lives Saved By Reducing Air Pollution in 31 Canadian Metropolitan Regions</h3>
 
 		<Totals/>
 
 		<p>
-			At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem reru[d]um facilis est e[r]t expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellend[a]us. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+			Specifically we simulated three different air pollution reduction scenarios: reducing PM2.5, reducing NO2, and reducing PM2.5 and NO2 simultaneously. We also modelled four variations in air pollution intervention scenarios:
 		</p>
+		<p>	
+			1) Meeting the Ambient Air Quality Standard: In this baseline scenario, we capped observed air pollution exposures to a maximum of 8.8 μg/m3 for annual mean PM2.5 and 12.0 ppb for annual mean NO2. These are the maximum annual average exposures set by Canadian Ambient Air Quality Standards.
+			
+		</p>
+		<p>	
+			2) 10% reduction + meeting the Ambient Air Quality Standard: In this scenario we reduced pollutant exposure levels by 10% for the entire population, and then capped exposure for anyone still above the standard to meet it. 
+		</p>
+		<p>	
+			3) 25% reduction + meeting the Ambient Air Quality Standard: In the third scenario we reduced pollutant exposure levels by 25% for the entire population, and then capped exposure for anyone still above the standard to meet it.
+		</p>
+		<p>	
+			4) 50% reduction + meeting the Ambient Air Quality Standard: In the third scenario we reduced pollutant exposure levels by 50% for the entire population, and then capped exposure for anyone still above the standard to meet it.
+		</p>
+
+		<!-- <br>
+		<br>
+		<br>
+		<br>
+		<br> -->
+
+		<p>
+			Air pollution levels and risk of premature mortality can vary by city. However, the overall predicted rates of premature death reflects the weighted average of predicted risk across all cities, masking city-specific patterns. To account for these variations, we also predicted city-specific rates of premature mortality and used Census population data to estimate the number of lives saved for each city.
+		</p>
+
+		<Results/>
+
+		<p>
+			Through strategic interventions such as transitioning to renewable energy sources, active travel and electrified public transit, and imposing strict emission standards on industrial facilities, Canada could improve its climate change resiliency and make significant improvements to public health. 
+		</p>
+
 
 		<h3>Air Pollution in Canadian Urban Regions</h3>
 		
+		<!-- <p>	
+			From the Canadian Ambient Air Quality Standards, maximum annual average exposures to 8.8 μg/m3 of PM2.5 and 12.0 ppb of NO2 are targets that help continuously improve air quality. 
+		</p> -->
 		<p>
-			Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. 
+			Across all CMAs, the observed average annual exposure of PM2.5 was 6.7 μg/m3 for males and females, and the observed average annual exposure of NO2 was 8.3ppb for females and 8.4ppb for males. However, exposure varies across regions. 206,000 females and 202,000 males live in areas which exceed the PM2.5 standard, while 437,000 females and 488,000 males live in areas which exceed the NO2 standard. 
 		</p>
+		<p>
+			The maps below show variations in exposure to PM2.5 and NO2 in 31 Canadian CMAs. Select from the dropdown to zoom to and show maps for different regions. 
+		</p>
+		
 		
 	</div>
 
 	<Map/>    
 
 	<div class="text">
+<!-- 
+		<p>
+			The greatest reductions in premature mortality (over a five-year period) can be seen when intervening on both PM2.5 and NO2 pollutants simultaneously. Furthermore, the more that air pollution was reduced (i.e., 10% or 25% plus capping), the more that premature deaths could be prevented.
+		</p> -->
+		<p>	
+			Notably, air pollution affects population demographics unequally. Areas of higher pollution exposure tend to have greater instability (i.e. decreased home security and ownership), a higher proportion of newcomers, and a lower proportion of employed and educated residents.
+		</p>
+		<p>
+			Pollution reductions would be particularly beneficial for individuals in neighbourhoods with greater socioeconomic deprivation, housing instability, and proportions of racial minorities and recent immigrants. Additionally, by simulating reductions of pollution even below the Canadian air quality standards, we can see how even greater health benefits could occur across the population. 
+		</p>
 
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-		<p>
-			Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-		</p>
 		
-		<Results/>
-
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-
-
-		<p>
-			At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem reru[d]um facilis est e[r]t expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellend[a]us. 
-		</p>
-
-		<p>
-			Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-		</p>
 
 
 		
